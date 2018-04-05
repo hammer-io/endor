a01793780ce6db7eb57628dddd097e79d016bbfd
Jack Meyer <jackcmeyer@gmail.com>
2018-04-04 23:47:01 -0500

    fixes issue with http data and os data urls being flipped

--- src/controllers/projects.controller.js
+++ src/controllers/projects.controller.js
@@ -283,8 +283,8 @@ async function addSkadi(projectId, configs) {
   configs.skadi.interval = '5000';
   configs.skadi.apiKey = apiKey;
   configs.skadi.heartbeatUrl = `${koma.url}/heartbeats`;
-  configs.skadi.httpDataUrl = `${koma.url}/osdata`;
-  configs.skadi.osDataUrl = `${koma.url}/httpdata`;
+  configs.skadi.httpDataUrl = `${koma.url}/httpdata`;
+  configs.skadi.osDataUrl = `${koma.url}/osdata`;
 }
 
 /**
